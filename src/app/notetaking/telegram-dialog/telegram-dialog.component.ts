e29380c2dcedbd501376cd5660fb1a70272c33ab
Yue Yang <lyueyang@u.nus.edu>
2020-07-25 23:23:30 +0800

    Link to Telegram Functionality Complete
    Button opens a new window with deeplink for Telegram Bot

--- src/app/notetaking/telegram-dialog/telegram-dialog.component.ts
+++ src/app/notetaking/telegram-dialog/telegram-dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
 import {TelegramHelperService} from './telegram-helper.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-telegram-dialog',
@@ -8,21 +8,24 @@ import {TelegramHelperService} from './telegram-helper.service';
   styleUrls: ['./telegram-dialog.component.css']
 })
 export class TelegramDialogComponent implements OnInit {
-  telegramForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder,
-              private submitService: TelegramHelperService) { }
+  constructor(private submitService: TelegramHelperService,
+              private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
-    this.telegramForm = this.formBuilder.group({
-      username: ''
-    });
   }
 
   startLink() {
-    this.submitService.submit(this.telegramForm.value).then(
-      response => console.log(response)
-    );
+    console.warn('link');
+    this.submitService.getToken().then(
+      (response) => {
+        window.open('https://t.me/planar_bot?start=' + response.toString());
+      },
+      (error) => {
+        this.snackBar.open('Error encountered, please try again!', 'Dismiss', {
+          duration: 3000
+        });
+      });
   }
 
   endLink() {
--- src/app/notetaking/telegram-dialog/telegram-helper.service.ts
+++ src/app/notetaking/telegram-dialog/telegram-helper.service.ts
@@ -12,11 +12,11 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class TelegramHelperService {
-  telegramUrl = '/planar/api/v1.0/telegram';
+  telegramUrl = '/planar/api/v1.0/get_tele_token';
 
   constructor(private http: HttpClient) { }
 
-  async submit(info): Promise<object> {
-    return await this.http.post(this.telegramUrl, JSON.stringify(info), httpOptions).toPromise();
+  async getToken(): Promise<object> {
+    return await this.http.get(this.telegramUrl, httpOptions).toPromise();
   }
 }
